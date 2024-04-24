7dd0429cb81f81bf63682b861dff7d723b239202
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-23 21:55:08 -0300

    terminei

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double primeiro, segundo;
+
+Console.Write("Digite o primeiro número..: ");
+primeiro = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o segundo número...: ");
+segundo = Convert.ToDouble(Console.ReadLine());
+
+if (primeiro > segundo)
+{
+    Console.WriteLine($"O maior número é {primeiro}");
+}
+else
+{
+    Console.WriteLine($"O maior número é {segundo}");
+}
